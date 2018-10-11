7e46e6fe0932c251b30485574f7e7cb3c4a9fe57
Christian Braybrooke <chris@purplemountmedia.com>
2018-10-11 22:36:22 +0100

    Schema and colour

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
   <div class="container text-center">
+    <h2>Contribute to the Cheshire Maverick's Dinner</h2>
+    <h5 class="mb-5">Upload your favourite Mav images!</h5>
     <file-upload-button api-url="{{ url('api') }}"
                         :button="{text: 'Upload Images'}">
     </file-upload-button>
