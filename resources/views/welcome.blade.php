1bff3d77e70bec542b47541d591367340e06c59f
Christian Braybrooke <chris@purplemountmedia.com>
2018-10-13 17:24:03 +0100

    Notice

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <h2>Contribute to the Cheshire Maverick's Dinner</h2>
     <h5 class="mb-5">Upload your favourite Mav images!</h5>
     <file-upload-component api-url="{{ url('api') }}"></file-upload-component>
+    <p>Images will be stored and used at the event. By uploading, you are giving consent for the images to be used.</p>
     <p>Powered by <a href="https://purplemountmedia.com" tile="Purple Mountain">Purple Mountain</a>, Wilmslow based Web Design & Build.</p>
   </div>
 @endsection
